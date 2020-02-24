89d54d17843c323aac959faa34842e707ef5ae61
RobinLinus <robinlinus@users.noreply.github.com>
2020-02-24 15:06:54 +0100

    Update bigint polyfill and tests

--- src/bigint-polyfill/bigint-polyfill.js
+++ src/bigint-polyfill/bigint-polyfill.js
@@ -1,29 +1,49 @@
-/**
- * Finite Field Arithmetic
- */
+/**********************************************************************
+ * Copyright (c) 2020 Robin Linus Woll                                *
+ * Distributed under the MIT software license, see the accompanying   *
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
+ **********************************************************************/
+
+'use strict';
+
+// We use 48 bit integers 
+// because 2 * 2**48 + 1 < Number.MAX_SAFE_INTEGER
+const MAX_INT = 2 ** 48
 
 export function fromHex(n) {
+
     // remove trailing "0x"
     n = n.replace(/^0x/, '')
 
-    // little endian conversion
-    // The least significant bit should be the lowest.
-    n = n.split('').reverse()
+    // pad input length to be a multiple of 12 
+    // because 48 bit / 4 bit/nibble = 12 nibble
+    if (n.length % 12) {
+        const length = n.length - n.length % 12 + 12
+        n = n.padStart(length, '0')
+    }
 
-    // cast to number
+    // split into chunks of size 12
+    n = n.match(/(.{1,12})/g).reverse()
+
+    // cast every chunk to a number
     return n.map(hex => parseInt(hex, 16))
 }
 
 export function toHex(n) {
-    return '0x' + n.map(x => x.toString(16)).reverse().join('')
+    const hex = n.map(x => x.toString(16).padStart(12, '0')).reverse().join('')
+    return '0x' + hex.replace(/^0+/, '')
 }
 
 export function toBinary(n) {
-    n = n.map(m => m.toString(2).padStart(4, '0').split('').map(Number).reverse())
-    n = n.flat()
+    n = n.map(m => m.toString(2).padStart(48, '0')).reverse().join('').replace(/^0+/, '')
     return n
 }
 
+/**
+ * 
+ * Big Integer Arithmetic
+ * 
+ */
 export function add(a, b) {
     const result = []
     let carryOver = 0
@@ -32,9 +52,9 @@ export function add(a, b) {
         const a_i = a[i] || 0
         const b_i = b[i] || 0
         const sum = (a_i + b_i + carryOver)
-        if (sum > 15) {
-            result[i] = sum - 16
-            carryOver = 1
+        if (sum > MAX_INT - 1) {
+            result[i] = sum - MAX_INT
+            carryOver = 1 
         } else {
             result[i] = sum
             carryOver = 0
@@ -57,7 +77,7 @@ export function sub(a, b) {
             result[i] = x - y
             carryOver = 0
         } else {
-            result[i] = 16 + x - y
+            result[i] = MAX_INT + x - y
             carryOver = 1
         }
     }
@@ -75,21 +95,52 @@ export function greater(a, b) {
     return false
 }
 
-export function mod_add(a, b, modulus) {
-    let result = add(a, b)
-    if (greater(result, modulus)) { // TODO: Here's a bug for a + b > 2 * modulus
-        result = sub(result, modulus)
+export function equals(a, b) {
+    const length = Math.max(a.length, b.length) - 1
+    for (let i = length; i >= 0; i--) {
+        const x = a[i] || 0
+        const y = b[i] || 0
+        if (x !== y) return false
     }
-    return result
+    return true
+}
+
+
+/**
+ * 
+ * Big Integer Modulo Arithmetic
+ * 
+ */
+
+export function mod_add(a, b, modulus) {
+    if (!greater(modulus, a)) { // TODO: Here's a bug for a + b > 2 * modulus
+        a = sub(a, modulus)
+    }
+    if (!greater(modulus, b)) { // TODO: Here's a bug for a + b > 2 * modulus
+        b = sub(b, modulus)
+    }
+    let result = add(a, b)
+    if (!greater(result, modulus)) { // TODO: Here's a bug for a + b > 2 * modulus
+        return result
+    }
+    return sub(result, modulus)
+}
+
+export function mod_sub(a, b, modulus) {
+    if (greater(a, b)) {
+        return sub(a, b)
+    } else {
+        return add(a, sub(modulus, b))
+    }
 }
 
 export function mod_mul(a, b, modulus) {
     b = toBinary(b)
     let result = []
 
-    for (let i = 0; i < b.length; i++) {
-        if (b[i]) {
-            result = mod_add(result, a, modulus)
+    for (let i = b.length - 1; i >= 0; i--) {
+        if (b[i] === '1') {
+            result = mod_add(a, result, modulus)
         }
         a = mod_add(a, a, modulus)
     }
@@ -101,8 +152,8 @@ export function mod_exp(a, b, modulus) {
     b = toBinary(b)
     let result = [1]
 
-    for (let i = 0; i < b.length; i++) {
-        if (b[i]) {
+    for (let i = b.length - 1; i >= 0; i--) {
+        if (b[i] === '1') {
             result = mod_mul(result, a, modulus)
         }
         a = mod_mul(a, a, modulus)
@@ -115,3 +166,175 @@ export function mod_inv(a, modulus) {
     const exp = sub(modulus, fromHex('0x02'))
     return mod_exp(a, exp, modulus)
 }
+
+
+export function mod_div(a, b, modulus) {
+    return mod_mul(a, mod_inv(b, FIELD_MODULUS), FIELD_MODULUS)
+}
+
+
+/**
+ *
+ *    The Finite Field for Secp256k1
+ * 
+ */
+
+const ELEM_0 = fromHex('0x00')
+const ELEM_1 = fromHex('0x01')
+const ELEM_2 = fromHex('0x02')
+const ELEM_3 = fromHex('0x03')
+const ELEM_4 = fromHex('0x04')
+
+const FIELD_MODULUS = fromHex('0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f')
+const FIELD_MODULUS_SQRT = fromHex('0x3fffffffffffffffffffffffffffffffffffffffffffffffffffffffbfffff0c')
+
+export function field_add(a, b) {
+    return mod_add(a, b, FIELD_MODULUS)
+}
+
+export function field_sub(a, b) {
+    return mod_sub(a, b, FIELD_MODULUS)
+}
+
+export function field_mul(a, b) {
+    return mod_mul(a, b, FIELD_MODULUS)
+}
+
+export function field_div(a, b) {
+    return mod_div(a, b, FIELD_MODULUS)
+}
+
+export function field_inv(a) {
+    return mod_inv(a, FIELD_MODULUS)
+}
+
+export function field_exp(a, b) {
+    return mod_exp(a, b, FIELD_MODULUS)
+}
+
+export function field_sqrt(a) {
+    return field_exp(a, FIELD_MODULUS_SQRT)
+}
+
+export function field_neg(a) {
+    return mod_add(a, FIELD_MODULUS, FIELD_MODULUS)
+}
+
+/**
+ *
+ *    The group of Curve Points of Secp256k1
+ * 
+ */
+const b = fromHex('0x07')
+
+const CURVE_GENERATOR = {
+    x: fromHex('0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798'),
+    y: fromHex('0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8')
+}
+
+const CURVE_ORDER = fromHex('0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141')
+
+export function to_jacobian(P) {
+    return [P.x, P.y, ELEM_1]
+}
+
+export function from_jacobian(p) {
+    const z = field_inv(p[2])
+    const z_2 = field_mul(z, z)
+    const z_3 = field_mul(z_2, z)
+    return { x: field_mul(p[0], z_2), y: field_mul(p[1], z_3) }
+}
+
+export function jacobian_double(p) {
+    if (equals(p[1], ELEM_0))
+        return [ELEM_0, ELEM_0, ELEM_1]
+    const ysq = field_mul(p[1], p[1])
+    const S = field_mul(field_mul(p[0], ysq), ELEM_4)
+    const M = field_mul(field_mul(p[0], p[0]), ELEM_3)
+
+    const nx = field_sub(field_mul(M, M), field_mul(S, ELEM_2))
+
+    const ny = field_sub(field_mul(M, field_sub(S, nx)), field_mul(field_mul(ysq, ysq), fromHex('0x08')))
+
+    const nz = field_mul(field_mul(p[1], ELEM_2), p[2])
+
+    return [nx, ny, nz]
+}
+
+export function jacobian_add(p, q) {
+    if (equals(p[1], ELEM_0))
+        return q
+    if (equals(q[1], ELEM_0))
+        return p
+
+    const p2_2 = field_mul(p[2], p[2])
+    const p2_3 = field_mul(p2_2, p[2])
+
+    const q2_2 = field_mul(q[2], q[2])
+    const q2_3 = field_mul(q2_2, q[2])
+
+    const U1 = field_mul(p[0], q2_2)
+    const U2 = field_mul(q[0], p2_2)
+    const S1 = field_mul(p[1], q2_3)
+    const S2 = field_mul(q[1], p2_3)
+
+    if (equals(U1, U2)) {
+        if (!equals(S1, S2))
+            return [ELEM_0, ELEM_0, ELEM_1]
+        else
+            return jacobian_double(p)
+    }
+
+    const H = field_sub(U2, U1)
+    const R = field_sub(S2, S1)
+    const H2 = field_mul(H, H)
+    const H3 = field_mul(H, H2)
+    const U1H2 = field_mul(U1, H2)
+    const nx = field_sub(field_sub(field_mul(R, R), H3), field_mul(U1H2, ELEM_2))
+    const ny = field_sub(field_mul(R, field_sub(U1H2, nx)), field_mul(S1, H3))
+    const nz = field_mul(field_mul(H, p[2]), q[2])
+    return [nx, ny, nz]
+}
+
+export function jacobian_multiply(P, s) {
+    if (equals(P[1], ELEM_0) || equals(s, ELEM_0))
+        return [ELEM_0, ELEM_0, ELEM_1]
+    if (s === 1)
+        return P
+
+    s = toBinary(s)
+    let result = [0, 0, 0]
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] === '1') {
+            result = jacobian_add(result, P)
+        }
+        P = jacobian_double(P)
+    }
+    return result
+}
+
+export function public_key(secret) {
+    const P = from_jacobian(jacobian_multiply(to_jacobian(CURVE_GENERATOR), secret))
+    return compress(P)
+}
+
+export function compress(P) {
+    const x = P.x
+    const orientation = x[0] % 2 ? '03' : '02'
+    return orientation + toHex(x).replace(/^0x/, '')
+}
+
+export function decompress(hex) {
+    const isEven = hex.substr(0, 2) === '03'
+    hex = hex.substr(2)
+    const x = fromHex(hex)
+
+    let y = field_sqrt(field_add(field_mul(field_mul(x, x), x), b))
+
+    if (isEven) {
+        y = field_neg(y)
+    }
+    return { x: x, y: y }
+}
+
--- src/bigint-polyfill/test.js
+++ src/bigint-polyfill/test.js
@@ -1,6 +1,6 @@
-import { fromHex, toHex, add, sub, mod_add, mod_mul, mod_exp, mod_inv } from './bigint-polyfill.js'
+import {toHex, fromHex, add, sub, mod_add, mod_mul, mod_inv, mod_exp, public_key, decompress, compress, equals} from './bigint-polyfill.js'
 
-describe('The bigint polyfill', function() {
+describe('The BigInt library', function() {
 
     it('can compute addition with overflow', function() {
 
@@ -39,7 +39,7 @@ describe('The bigint polyfill', function() {
 
         const result = toHex(sub(fromHex(a), fromHex(b)))
 
-        expect(result).toBe('0x00000000000000')
+        expect(result).toBe('0x')
     })
 
     it('can compute subtraction', function() {
@@ -58,7 +58,7 @@ describe('The bigint polyfill', function() {
 
         const result = toHex(sub(a, b))
 
-        expect(result).toBe('0x000000000000000000000000000000014551231950b75fc4402da1722fc9baee')
+        expect(result).toBe('0x14551231950b75fc4402da1722fc9baee')
     })
 
     it('can add and then subtraction larger numbers', function() {
@@ -67,7 +67,7 @@ describe('The bigint polyfill', function() {
 
         const result = toHex(sub(add(a, b), b))
 
-        expect(result).toBe('0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f')
+        expect(result).toBe('0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f')
     })
 
     it('can compute modular addition', function() {
@@ -88,7 +88,17 @@ describe('The bigint polyfill', function() {
 
         const result = toHex(mod_add(a, b, p))
 
-        expect(result).toBe('0x0fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd03641bc')
+        expect(result).toBe('0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd03641bc')
+    })
+
+    it('can compute modular addition of larger numbers', function() {
+        const a = fromHex('0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f')
+        const b = fromHex('0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f')
+        const p = fromHex('0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f')
+
+        const result = toHex(mod_add(a, b, p))
+
+        expect(result).toBe('0x')
     })
 
     it('can compute modular addition with zero', function() {
@@ -109,9 +119,8 @@ describe('The bigint polyfill', function() {
 
         const result = toHex(mod_mul(a, b, p))
 
-        expect(result).toBe('0x0120000000000000000000000000000003bb9e571a5d1aa9507c860a3e4c6090d')
+        expect(result).toBe('0x120000000000000000000000000000003bb9e571a5d1aa9507c860a3e4c6090d')
     })
-
 
     it('can compute modular exponentiation', function() {
         const a = fromHex('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC')
@@ -120,7 +129,7 @@ describe('The bigint polyfill', function() {
 
         const result = toHex(mod_exp(a, b, p))
 
-        expect(result).toBe('0x030c1194adcab3aea0d6e1399b3ed5e00755ac148f3bae801f9b65aadead3e9d4')
+        expect(result).toBe('0x30c1194adcab3aea0d6e1399b3ed5e00755ac148f3bae801f9b65aadead3e9d4')
     })
 
     it('can compute modular inverse', function() {
@@ -131,7 +140,29 @@ describe('The bigint polyfill', function() {
 
         const result = toHex(mod_mul(a, inv, p))
 
-        expect(result).toBe('0x00000000000000000000000000000000000000000000000000000000000000001')
+        expect(result).toBe('0x1')
     })
 
+})
+
+
+describe('Secp256k1', function() {
+
+    it('can compute curve points', function() {
+        const secret = fromHex('0x18e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725')
+
+        const result = public_key(secret)
+
+        expect(result).toBe('0250863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352')
+    })
+
+    it('can compress and decompress curve points', function() {
+        const secret = fromHex('0x28e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a20632178')
+
+        const compressed = public_key(secret)
+        const decompressed = decompress(compressed)
+        const result = compress(decompressed)
+
+        expect(equals(result, compressed)).toBe(true)
+    })
 })
